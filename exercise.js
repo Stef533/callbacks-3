e0ba86d2020cfc360a5055df15d4cc7e8e3e4fb6
Stef533 <137123241+Stef533@users.noreply.github.com>
2023-10-07 16:22:50 +0200

    Add files via upload

---
+++ exercise.js
@@ -0,0 +1,14 @@
+const repeatHello = (callback) => {
+    const intervalId = setInterval(callback, 1000);
+  
+    setTimeout(() => {
+      clearInterval(intervalId);
+    }, 5000);
+  };
+  
+  const helloCallback = () => {
+    console.log('Hello');
+  };
+  
+  repeatHello(helloCallback);
+  
